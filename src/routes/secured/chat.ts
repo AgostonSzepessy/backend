b0d4244ba825da1979ead90bc2cd17990911b028
Stuart Wilcox <stuart_wilcox@outlook.com>
2019-01-22 23:38:42 -0500

    added documentation to existing routes

--- src/routes/secured/chat.ts
+++ src/routes/secured/chat.ts
@@ -14,7 +14,7 @@ module.exports = (router: express.Router) => {
     const event_id = req.body.event_id;
 
     if(!name || !event_id){
-      throw new MovnetError(400, 'All fields must be filled out');
+      throw new MovnetError(422, 'All fields must be filled out');
     }
 
     const chat = await ChatService.add(name, event_id);
@@ -31,7 +31,7 @@ module.exports = (router: express.Router) => {
     const message_text = req.body.message_text;
 
     if(!chat_id || !username || !message_text){
-      throw new MovnetError(400, 'All fields must be filled out');
+      throw new MovnetError(422, 'All fields must be filled out');
     }
 
     const message = await ChatService.addMessage(chat_id, username, message_text);
